c24254f1503d98083852a3641618982ce31e51f4
abdugafar768 <abdugafar_04@icloud.com>
2024-04-12 10:52:19 +0500

    add urls change it

--- faq/urls.py
+++ faq/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('questions/', CustomerQuestionListCreateAPIView.as_view(), name='question_list_create'),
-    path('questions/<int:question_id>/respond/', ProductOwnerResponseCreateAPIView.as_view(), name='respond_question'),
+    path('questions/<int:pk>/respond/', ProductOwnerResponseCreateAPIView.as_view(), name='respond_question'),
 ]
